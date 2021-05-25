fd127f6bd8f366a9a46d0ea4fb38f7e35a9dae2d
Requette <reesegick@yahoo.com>
2021-05-25 12:12:35 -0700

    New update.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace JobCandidate
         {
             Console.WriteLine("How many years of experience do you have?");
 
-            var years = int.Parce(Console.ReadLine());
+            var years = int.Parse(Console.ReadLine());
 
             if (years == 0)
                 Console.WriteLine("Inexperienced");
@@ -16,14 +16,15 @@ namespace JobCandidate
             else if (years == 1)
                 Console.WriteLine("Junior");
 
-            else if (years == 3)
-                Console.WriteLine("Mid-Level");
+            else if (years == 2)
+                Console.WriteLine("Mid Level");
 
-            else if (years == 5)
+            else if (years == 3)
+                Console.WriteLine("Advanced");
+
+            else if (years >= 4)
                 Console.WriteLine("Senior");
 
-            else (years >= 10);
-            Console.WriteLine("Executive");
         }
     }
 }
