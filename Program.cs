769ea01e1f25d974a9a886700630f38674ba769d
Requette <reesegick@yahoo.com>
2021-05-25 12:01:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have?");
+
+            var years = int.Parce(Console.ReadLine());
+
+            if (years == 0)
+                Console.WriteLine("Inexperienced");
+
+            else if (years == 1)
+                Console.WriteLine("Junior");
+
+            else if (years == 3)
+                Console.WriteLine("Mid-Level");
+
+            else if (years == 5)
+                Console.WriteLine("Senior");
+
+            else (years >= 10);
+            Console.WriteLine("Executive");
+        }
+    }
+}
